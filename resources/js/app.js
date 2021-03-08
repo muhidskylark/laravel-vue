210635e8d205758b7452ac967ad9c21954c87e6c
Muhid <muhid@skylarksoft.com>
2021-03-08 17:51:40 +0600

    Added Vue And Laravel

---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import App from './components/HelloDevelopers.vue'
+new Vue({
+    render: h=>h(App)
+}).$mount('#app')
---
+++ resources/js/components/HelloDevelopers.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            name: 'muhit'
+        })
+
+    }
+</script>
